05bb29e608d25b9eac686456652866bb266fa0cd
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2023-09-03 16:17:32 +0300

    PyCharm1
    PyThone

---
+++ main.py
@@ -0,0 +1,19 @@
+print('To be')
+print('or not')
+print('to be')
+
+
+print('"Life is what happens ')
+print('when')
+print('you are busy macking other plans" ')
+print('John Lennon')
+
+print('Nothing')
+print('will work')
+print('unless you do')
+
+print('"Anyone who')
+print('stops')
+print('learning is old')
+print('wheather at twenty or eigthy"')
+print('Henry Ford')
